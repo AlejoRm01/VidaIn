e1bb9a9e6c75c81ec3e515b5f877dc13a8a41918
Alejandro Rodriguez Muñoz <arodriguem@eafit.edu.co>
2022-09-20 10:03:29 -0500

    Fix layout app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
         <div class="container">
             <a class="navbar-brand" href="{{ route('home.index') }}">VidaIn</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs- target="#navbarNavAltMarkup"
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"> 
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -23,12 +23,14 @@
                 <div class="navbar-nav ms-auto">
                     <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
                     <a class="nav-link active" href="{{ route('us.index') }}">Nosotros</a>
+                    <div class="vr bg-white mx-2 d-none d-lg-block"></div>
                     @guest
-                        <a class="nav-link active" href="{{ route('login') }}">Ingresar</a>
+                        <a class="nav-link active" href="{{ route('login') }}">Iniciar sesión</a>
                     </div>
                             @csrf
                         </form>
                     @endguest
+                        
                 </div>
             </div>
         </div>
@@ -53,5 +55,6 @@
     <!-- footer -->
 
 </body>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+</script>
 </html>
