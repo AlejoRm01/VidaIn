f6c729e89809b5654bce6f004e4085c628cd46a5
Alejandro Rodriguez Muñoz <arodriguem@eafit.edu.co>
2022-09-26 19:22:45 -0500

    Fixed route login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,9 +53,9 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn bg-primary text-white">
+                                <a type="submit" class="btn bg-primary text-white" href="{{ route('aliment.index') }}">
                                     {{ __('Ingresar') }}
-                                </button>
+                                </a>
 
                                 @if(Route::has('register')) 
                                     <a class="btn bg-primary text-white" href="{{ route('register') }}">
