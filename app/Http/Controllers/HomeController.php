99ca67ad55312befe0352852296f0e5c6d6e3fb7
yulibp04 <yolayab@eafit.edu.co>
2022-09-26 22:22:39 -0500

    cambio foto equipo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ class HomeController extends Controller
     public function index()
     {
         $viewData = [];
-        $viewData['title'] = 'Home Page - VidaIn';
+        $viewData['title'] = 'Home Page - Senut';
         $viewData['subtitle'] = '';
 
         return view('home.index')->with('viewData', $viewData);
@@ -18,17 +18,10 @@ class HomeController extends Controller
         $viewData = [];
         $viewData['title'] = 'Sobre nosotros';
         $viewData['subtitle'] = 'Integrantes';
-        $viewData['integrantes'] = HomeController::$integrantes;
+        $viewData['integrantes'] = HomeController::$photo;
 
         return view('home.about')->with('viewData', $viewData);
     }
+    public static $photo= "/photo/groupphoto.jpg";
 
-    public static $integrantes = [
-        ['src' => '/photo/1.jpeg', 'name' => 'Izabella Valencia'],
-        ['src' => '/photo/2.jpeg', 'name' => 'Manuela Restrepo'],
-        ['src' => '/photo/3.jpeg', 'name' => 'Yuliana Olaya'],
-        ['src' => '/photo/4.jpeg', 'name' => 'Alejandro Rodriguez'],
-        ['src' => '/photo/5.jpeg', 'name' => 'Mateo Ángel'],
-        ['src' => '/photo/6.jpeg', 'name' => 'Santiago Díaz'],
-    ];
 }
--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -4,14 +4,7 @@
 @section('content')
 
 <div class="container">
-    <div class="row">
-    @foreach ($viewData['integrantes'] as $integrante)
-        <div class="col">
-            <img    class="img-thumbnail" src="{{  asset($integrante['src'])  }}" id="imagenPrinc">
-            <h5 class="card-title text-center">{{ $integrante['name'] }}</h5>
-        </div>
-        @endforeach
-    </div>
+    <img scr="{{$viewData['integrantes']}}">
 </div>
 
 @endsection
