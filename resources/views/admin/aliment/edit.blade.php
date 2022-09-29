cdf93f5deb58ca550395aa328c4045c097799da0
Alejandro Rodriguez Muñoz <arodriguem@eafit.edu.co>
2022-09-29 00:44:35 -0500

    Fixed bug colesterol

--- resources/views/admin/aliment/edit.blade.php
+++ resources/views/admin/aliment/edit.blade.php
@@ -111,7 +111,7 @@
                     <div class="mb-3 row">
                         <label class="col-lg-2 col-md-6 col-sm-12 col-form-label">Cholesterol:</label>
                         <div class="col-lg-10 col-md-6 col-sm-12">
-                            <input name="cholesterol" value=" {{ $viewData['aliment']->getCholesterol() }}" type="number" class="form-control">
+                            <input name="cholesterol" value=" {{ $viewData['aliment']->getCholesterol() }}" type="text" class="form-control">
                         </div>
                     </div>
                 </div>
